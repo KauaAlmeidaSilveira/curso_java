ab61975b033192ff1315d409aadfd03ef04a7744
Kauã Almeida Silveira <kauiis.a.silveira@gmail.com>
2021-09-02 10:05:54 -0300

    Solução com interfaces, seguindo uma ideia de HerançaMultipla

--- application-SRC/Interfaces/Exercicio04.java
+++ application-SRC/Interfaces/Exercicio04.java
@@ -1,19 +1,29 @@
 package Interfaces;
 
-import entInterfaces.Printer;
-import entInterfaces.Scanner;
+import entInterfaces.ComboDevice;
+import entInterfaces.ConcretePrinter;
+import entInterfaces.ConcreteScanner;
 
 public class Exercicio04 {
 
 	public static void main(String[] args) {
 		
-		Printer p = new Printer("1080");
+		ConcretePrinter p = new ConcretePrinter("1080");
 		p.processDoc("My letter");
 		p.print("My Letter");
+		
+		System.out.println();
 
-		Scanner s = new Scanner("2003");
+		ConcreteScanner s = new ConcreteScanner("2003");
 		s.processDoc("My Email");
 		System.out.println("Scan result: "+ s.scan());
+	
+		System.out.println();
+		
+		ComboDevice c = new ComboDevice("2081");
+		c.processDoc("My dissertation");
+		c.print("My dissertation");
+		System.out.println("Combo scan result: " + c.scan());
 		
 	}
 
--- entities/entInterfaces/ComboDevice.java
+++ entities/entInterfaces/ComboDevice.java
@@ -1,10 +1,28 @@
-/*package entInterfaces;
+package entInterfaces;
 
-public class ComboDevice extends Scanner, Printer {
+public class ComboDevice extends Device implements Scanner, Printer{
 
 	public ComboDevice(String serialNumber) {
 		super(serialNumber);
+		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public void print(String doc) {
+		System.out.println("Combo Printing: " + doc);
+	}
+
+	@Override
+	public String scan() {
+		return "Combb scan result";
+	}
+
+	@Override
+	public void processDoc(String doc) {
+		System.out.println("Combo processing: " + doc);
+	}
+
+
 	
 }
-*/
+
