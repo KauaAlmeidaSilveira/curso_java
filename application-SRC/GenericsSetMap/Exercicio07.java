2031ea0e696a3fb48a0a755c49436ebe9c7bd6f9
Kauã Almeida Silveira <kauiis.a.silveira@gmail.com>
2021-09-10 09:10:19 -0300

    Utilização de hashCode e Equals

---
+++ application-SRC/GenericsSetMap/Exercicio07.java
@@ -0,0 +1,29 @@
+package GenericsSetMap;
+
+import entGenericsSetMap.Client;
+
+public class Exercicio07 {
+
+	public static void main(String[] args) {
+		
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		
+		Client c2 = new Client("Maria", "maria@gmail.com");
+		
+		String s1 = "test";
+		
+		String s2 = "test";
+		
+		System.out.println(c1.hashCode());
+		
+		System.out.println(c2.hashCode());
+		
+		System.out.println(c1.equals(c2));
+	
+		System.out.println(c1 == c2);
+		
+		System.out.println(s1 == s2);
+		
+	}
+
+}
